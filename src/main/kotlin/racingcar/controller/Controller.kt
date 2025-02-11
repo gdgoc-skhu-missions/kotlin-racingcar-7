5d6b72330e34753d03704585889d21c12fa1b959
choi-day <choisschoco@gmail.com>
2025-02-11 11:23:47 +0900

    feat: 차 이름 구분, 랜덤값 반환, 전진 여부 결정 함수 작성

---
+++ src/main/kotlin/racingcar/controller/Controller.kt
@@ -0,0 +1,20 @@
+package racingcar.controller
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class Controller {
+    fun splitCar(carName : String) : List<String> {
+        return carName.split(",")
+    }
+
+    fun getRandomNumber() : Int {
+        return Randoms.pickNumberInRange(0,9)
+    }
+
+    fun getPosition() : Boolean {
+        if (getRandomNumber() >= 4) {
+            return true
+        }
+        return false
+    }
+}
