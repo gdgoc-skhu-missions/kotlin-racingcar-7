a73092b87b73e844970224ded5c2f9476746097a
choi-day <choisschoco@gmail.com>
2025-02-12 06:06:20 +0900

    refactor : 함수를 단일표현식으로

--- src/main/kotlin/racingcar/view/Output.kt
+++ src/main/kotlin/racingcar/view/Output.kt
@@ -1,19 +1,14 @@
 package racingcar.view
 
 object Output {
-    fun printGetName() {
-        print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    fun printGetName() = println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    fun printGetNum() = println("시도할 횟수는 몇 회인가요?")
+    fun printResult() = println("실행결과")
+    fun printPosition(playerList: List<String>) {
+        for (player in playerList) {
+            printRoundResult()
+        }
     }
-    fun printGetNum() {
-        print("시도할 횟수는 몇 회인가요?")
-    }
-    fun printResult() {
-        print("실행결과")
-    }
-    fun printPosition() {
-
-    }
-    fun printWinner(winnerList : List<String>) { //리스트를 String으로 받아도 될 지 모르겠는데 일단 함
-        print(winnerList.joinToString(",")) //winner리스트를 ,로 구분된 문자열로 만들어 print
-    }
+    fun printRoundResult() = print("-")
+    fun printWinner(winnerList : List<String>) = println("최종 우승자 : ${winnerList.joinToString(",")}")
 }
