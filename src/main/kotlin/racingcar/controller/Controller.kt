d28a28af758e7e54003ac43876dde5f23162ee18
hyeminililo <ng040115@gmail.com>
2025-02-11 23:11:55 +0900

    feat : 랜덤 숫자 생성하는 함수 작성

--- src/main/kotlin/racingcar/controller/Controller.kt
+++ src/main/kotlin/racingcar/controller/Controller.kt
@@ -6,4 +6,8 @@ class Controller {
     fun splitCar(carName : String) : List<String> {
         return carName.split(",")
     }
+
+    fun getRandomNumber() : Int {
+        return Randoms.pickNumberInRange(0,9)
+    }
 }
