5dd5f9fb81230254671eefe5fc0f37cc9da7f5cf
hyeminililo <ng040115@gmail.com>
2025-02-13 01:42:02 +0900

    style : 전체 주석 제거

--- src/main/kotlin/racingcar/controller/Controller.kt
+++ src/main/kotlin/racingcar/controller/Controller.kt
@@ -5,24 +5,23 @@ import camp.nextstep.edu.missionutils.Randoms
 
 object Controller {
 
-    //  fun splitCarName(carName : String) : List<String> = carName.split(",")
     private fun getRandomNumber(): Int = Randoms.pickNumberInRange(0, 9)
 
-    private fun setPosition(car: Car): Car { //전진 여부 결정 함수
+    private fun setPosition(car: Car): Car {
         val currentRound = getRandomNumber()
 
-        if (currentRound >= 4) { // 전진이 4이상이면
+        if (currentRound >= 4) {
             car.move()
             return car
         }
         return car
     }
 
-    fun repeatRacing(carList: List<Car>) { // 카 리스트를 반복해서 레이싱 경주 시키기
+    fun repeatRacing(carList: List<Car>) {
         carList.forEach { car -> setPosition(car); }
     }
 
-    private fun getWinnerScore(carList: List<Car>): Int { // 위너 점수 저장
+    private fun getWinnerScore(carList: List<Car>): Int {
         val score = ArrayList<Int>()
         carList.forEach { car -> score.add(car.position) }
         return score.max()
