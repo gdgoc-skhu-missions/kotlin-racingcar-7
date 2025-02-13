4ac7d2b85632e3a7a453e4db7b23f5247adf5f72
hyeminililo <ng040115@gmail.com>
2025-02-13 23:20:43 +0900

    test : 기본 테스트 성공

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,19 +4,21 @@ import Car
 import racingcar.view.Input
 import racingcar.view.Output
 import racingcar.controller.Controller
+import racingcar.util.Validator
 
 fun main() {
     Output.printGetName()
     val carNameList = Input.getName().split(",")
-
+    Validator.checkBlank(carNameList)
+    Validator.checkNameLength(carNameList)
     Output.printGetNum()
     val num = Input.getNum()
     val carList = ArrayList<Car>()
 
     Output.printResultMessage()
-    carNameList.forEach{name -> carList.add(Car(name))}
+    carNameList.forEach { name -> carList.add(Car(name)) }
 
-    for(i in 0..num-1){
+    for (i in 0..num - 1) {
         Controller.repeatRacing(carList)
         Output.printPosition(carList)
     }
